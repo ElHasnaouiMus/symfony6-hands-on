314ca861560bd95881ee987f96dd4a105b3e7e87
Mustapha EL HASNAOUI <elhasnaouimus@gmail.com>
2025-02-14 16:34:54 +0100

    handling-form-submission

--- app/src/Controller/MicroPostController.php
+++ app/src/Controller/MicroPostController.php
@@ -8,6 +8,8 @@ use App\Repository\MicroPostRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 class MicroPostController extends AbstractController
 {
@@ -24,6 +26,32 @@ class MicroPostController extends AbstractController
     {
         return $this->render('micro_post/show.html.twig', [
             'post' => $post,
+        ]);
+    }
+
+    #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2)]
+    public function add(Request $request, MicroPostRepository $posts) : Response
+    {
+        $post = new MicroPost();
+        $form = $this->createFormBuilder($post)
+            ->add('title')
+            ->add('text')
+            ->add('submit', SubmitType::class, [
+                'label' => 'Create Post',
+            ])
+            ->getForm();
+        
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $post->setCreated(new DateTime());
+            $posts->add($post, true);
+
+            // add flash message
+            // Redirect to the show page
+        }
+        return $this->renderForm('micro_post/add.html.twig', [
+            'form' => $form,
         ]);
     }
 }
